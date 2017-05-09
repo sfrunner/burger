306e2ee14391b50c8fec74c3682d8050679e81e7
dblanchard13 <davidb.prof@gmail.com>
2017-05-09 15:43:39 -0700

    Workaround

--- public/JS/app.js
+++ public/JS/app.js
@@ -3,17 +3,19 @@
         var burgerData = {
             name: $("#burger-text").val().trim()
         };
-        $.post("/", burgerData).done(function (data) {
-            getCall();
-        });
-    });
 
-    function getCall() {
-        $.get("/").done(function (data) {
+        // don't need to change anything and this will work.
+        $.post("/", burgerData).done(function (data) {
             var HTML = $.parseHTML(data, document, true);
             var bodyHTML = HTML[15].innerHTML
-            console.log(HTML);
             $(".container-fluid").html(bodyHTML);
         });
-    }
+
+        // if you send back a path to redirect to, then you can do this (bit simpler than the above option)
+        // $.post("/", burgerData).done(function (data) {
+        //     data.redirect && ( window.location = data.redirect )
+        // });
+
+    });
+
 });
